b50e16c56f2f5a68a008589320d0da19276373e9
pavansta <pavan.learn69@gmail>
2022-01-12 20:39:49 +0530

    initial commit

---
+++ class_time_table/lectures/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Lecture
+
+
+admin.site.register(Lecture)
---
+++ class_time_table/website/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+def welcome(req):
+    return render(req, "website/welcome.html")
+
+
+
